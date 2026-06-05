a3383b52eb3051f6a763e6cc19e374bb7a1a554c
Karl <kschultze@seaevolve.com>
2026-06-05 11:44:45 -0700

    Setup test automation infrastructure

--- test_sample_class.py
+++ test_sample_class.py
@@ -3,13 +3,13 @@ from sample_class import TestAccount
 
 def testDisplayVar1():
     test = TestAccount("test",1.0)
-    assert test.displayVar1 == "Var 1:test"
+    assert test.displayVar1() == "Var 1:test"
 
 def testDisplayVar2():
     test = TestAccount("asdf",1.0)
-    assert test.displayVar2 == "Var2: 1.0"
+    assert test.displayVar2() == "Var2: 1.0"
 
 def testIncrement():
     test = TestAccount("test",1.0)
-    assert test.incrementVar2 == "Var2 is now: 2.0"
+    assert test.incrementVar2() == "Var2 is now: 2.0"
 
